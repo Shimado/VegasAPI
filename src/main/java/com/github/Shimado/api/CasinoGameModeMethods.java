4b4e40f400e96fbcbc8f74d3388325513d3db76e
BuildTools <unconfigured@null.spigotmc.org>
2025-09-13 23:10:54 +0300

    Fixed methods

--- src/main/java/com/github/Shimado/api/CasinoGameModeMethods.java
+++ src/main/java/com/github/Shimado/api/CasinoGameModeMethods.java
@@ -1,5 +1,6 @@
 package com.github.Shimado.api;
 
+import com.github.Shimado.interfaces.ISession;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -28,18 +29,20 @@ public interface CasinoGameModeMethods {
      * Here you can specify the return of the bet if the session has not started.
      * And the deletion of the player's data from the session.
      *
-     * @param e the inventory close event
+     * @param player the player who closed the game inventory
      */
 
-    void closeGUI(@Nonnull InventoryCloseEvent e);
+    void closeGUI(@Nonnull Player player);
 
     /**
      * Handles logic when the player interacts with the game GUI (clicking items, etc.).
      *
-     * @param e the inventory click event
+     * @param e      the inventory click event
+     * @param player the player who clicked
+     * @param session the inventory session with some data
      */
 
-    void clickGUI(@Nonnull InventoryClickEvent e);
+    void clickGUI(@Nonnull InventoryClickEvent e, @Nonnull Player player, @Nonnull ISession session);
 
     /**
      * Reloads the game mode, refreshing configuration or resetting state if needed.
